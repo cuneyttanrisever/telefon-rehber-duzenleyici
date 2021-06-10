4d0adcbd623da774e0e7a0467b82f91d549b7e09
Cüneyt Tanrısever <55285425+cuneyttanrisever@users.noreply.github.com>
2021-06-10 22:19:01 +0300

    Update rehber_duzenleyici.py

--- rehber_duzenleyici.py
+++ rehber_duzenleyici.py
@@ -5,13 +5,11 @@ print("""
 # Coded : Cüneyt TANRISEVER #
 #############################
 """)
-oku=open("rehber.vcf","r").read()
+oku=open("a.vcf","r").read()
 bol=oku.split("BEGIN:VCARD")
 #print (bol[2])
 liste=[]
 yaz=open("yeni.vcf","a")
-yaz1=open("yeni1.vcf","w")
-yaz1.close()
 print (len(bol))
 for i in bol:
     if "CELL" in i:
@@ -19,14 +17,14 @@ for i in bol:
             liste.append(i)
 #print (len(liste))
 for i in liste:
+    
     yaz.write(str("BEGIN:VCARD\n")+i+str("\n"))
-yaz2=open("yeni.vcf","r").readlines()
+oku2=open("yeni.vcf","r").readlines()
 yaz1=open("yeni1.vcf","a")
-for i in yaz2:
+for i in oku2:
     if "\n"!=i:
        yaz1.write(i)
 yaz.close()
 yaz1.close()
-yaz2=[]
 os.system("ERASE yeni.vcf")
 os.system("ren yeni1.vcf yeni.vcf")
